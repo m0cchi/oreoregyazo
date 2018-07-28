156cc2a80c8fcded02329660cf69ad1266282155
m0cchi <boom.boom.planet@gmail.com>
2018-07-28 18:05:15 +0900

    support kafka-chat

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, request, abort, send_from_directory
 from uuid import uuid4
 from urllib.parse import urljoin
 import os
+from flask_cors import CORS
+from kafka import KafkaProducer
 
 app = Flask(__name__)
 ENV_FORMAT = 'OREOREGYAZO_{}'
 app.config['IMAGE_URL'] = os.getenv(ENV_FORMAT.format('URL'), 'http://localhost:3000/images/')
 app.config['IMAGE_DIR'] = os.getenv(ENV_FORMAT.format('DIR'), './images')
+CORS(app, resources={'/images/*': {'origins': os.getenv(ENV_FORMAT.format('ALLOW_CORS_HOST', 'localhost'))}})
+
+producer = KafkaProducer(bootstrap_servers=os.getenv('KAFKA_HOST'))
 
 @app.route('/')
 def index():
@@ -29,7 +34,9 @@ def save():
         if count > 10:
             abort(500, "can't create file")
     f.save(filepath)
-    return urljoin(app.config['IMAGE_URL'], os.path.basename(filepath))
+    url = urljoin(app.config['IMAGE_URL'], os.path.basename(filepath))
+    producer.send('image', url.encode('utf-8'))
+    return url
 
 if __name__ == '__main__':
     app.debug = os.getenv(ENV_FORMAT.format('DEBUG'), 'DEBUG') == 'DEBUG'
